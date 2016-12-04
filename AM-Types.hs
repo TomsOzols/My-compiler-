69a83b808cfca466e82a3e32fb7e566a12a0638a
Toms Ozols <Toms Ozols>
2016-12-05 00:42:39 +0200

    Small clean up

--- AM-Types.hs
+++ AM-Types.hs
@@ -5,4 +5,4 @@ data Command =  PUSH Float | ADD | MULT | SUB | DIV
                 | FETCH String | STORE String | NOOP 
                 | BRANCH [Command] [Command]
                 | LOOP [Command] [Command]
-                | END -- A dirty hack for the interpreter
+                
